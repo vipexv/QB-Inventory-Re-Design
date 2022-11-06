b8f950ed3a6fe8d66acdf49044908e3cf24abeaf
vipex <101529155+vipexee@users.noreply.github.com>
2022-11-06 19:03:22 +0100

    Main Update.

---
+++ qb-inventory/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Inventory Re-Designed by vipex#2027'
+version '1.1.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
+    '@qb-weapons/config.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+client_script 'client/main.lua'
+
+ui_page {
+    'html/ui.html'
+}
+
+files {
+    'html/ui.html',
+    'html/css/main.css',
+    'html/js/app.js',
+    'html/images/*.png',
+    'html/images/*.jpg',
+    'html/ammo_images/*.png',
+    'html/attachment_images/*.png',
+    'html/*.ttf'
+}
+
+lua54 'yes'
